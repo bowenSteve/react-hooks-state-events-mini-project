2154f32bb20e1eed371fad6aad043deb747057b2
Stephen Bowen <bowensteven17@gmail.com>
2024-04-30 11:02:14 +0300

    complete sate events mini project lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
-  return (
+
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [deletedList, setDeleted] = React.useState(tasks);
+  
+
+  const handleDelete = (deleteTask) => {
+    console.log(deleteTask)
+    const updatedTasks = tasks.filter(task => deleteTask !== task.text);
+    setTasks(updatedTasks);
+    setDeleted(updatedTasks)
+  };
+  const filteredTasks = tasks.filter((task) => selectedCategory === "All" || task.category === selectedCategory);
+
+  function onTaskFormSubmit(newObj) {
+    setTasks(prevVal => {
+      setDeleted(prevVal => [...prevVal, newObj])
+      return [...prevVal, newObj]
+    })
+    return newObj;
+  }
+
+return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTasks} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
+  const displayTasks = tasks.map(task => {
+    return <Task text={task.text} category={task.category} key={task.id} handleDelete={handleDelete} />;
+  });
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {displayTasks}
     </div>
   );
 }
